2acd9e260578dcc59d8ca9104f744b83bfb54304
Paweł Rudnik <pawelrudnik3@gmail.com>
2024-06-15 18:57:55 +0200

    Initial Commit

---
+++ Praca-Inz-UI/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  Praca-Inz-UI
+//
+//  Created by Paweł Rudnik on 15/06/2024.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ Praca-Inz-UI/Praca_Inz_UIApp.swift
@@ -0,0 +1,17 @@
+//
+//  Praca_Inz_UIApp.swift
+//  Praca-Inz-UI
+//
+//  Created by Paweł Rudnik on 15/06/2024.
+//
+
+import SwiftUI
+
+@main
+struct Praca_Inz_UIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
